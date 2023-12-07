386edfb59efdfa88f3cd8373277cf5722878405b
ozkanberkee <ozkanberke1305@gmail.com>
2023-12-07 18:15:54 +0300

    Fill Form Automation

---
+++ src/main/java/excelproject/DataProvide.java
@@ -0,0 +1,71 @@
+package excelproject;
+
+
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.*;
+import org.testng.annotations.DataProvider;
+
+public class DataProvide {
+    DataFormatter formatter = new DataFormatter();
+
+    @DataProvider(name = "driveTest")
+    public Object[][] getData() throws IOException {
+        FileInputStream fis = null;
+        XSSFWorkbook wb = null;
+        try {
+            fis = new FileInputStream("C:\\wufoo\\wufoo_upload.xlsx");
+            wb = new XSSFWorkbook(fis);
+            XSSFSheet sheet = wb.getSheetAt(0);
+            int rowCount = sheet.getPhysicalNumberOfRows();
+            XSSFRow row = sheet.getRow(0);
+            int colCount = row.getLastCellNum();
+            Object data[][] = new Object[rowCount - 1][colCount];
+
+            for (int i = 0; i < rowCount - 1; i++) {
+                row = sheet.getRow(i + 1);
+
+                for (int j = 0; j < colCount; j++) {
+                    XSSFCell cell = row.getCell(j);
+                    data[i][j] = formatter.formatCellValue(cell);
+                }
+            }
+
+            return data;
+        } finally {
+            if (wb != null) {
+                wb.close();
+            }
+            if (fis != null) {
+                fis.close();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException, IOException {
+        FormTest formTest = new FormTest();
+        DataProvide dataProvider = new DataProvide();
+        Object[][] testData = dataProvider.getData();
+        for (Object[] data : testData) {
+            formTest.fillForm(
+                    data[0].toString(),
+                    data[1].toString(),
+                    data[2].toString(),
+                    data[3].toString(),
+                    data[4].toString(),
+                    data[5].toString(),
+                    data[6].toString(),
+                    data[7].toString(),
+                    data[8].toString(),
+                    data[9].toString(),
+                    data[10].toString()
+                    
+            );
+        }
+        System.exit(1);
+
+    }
+}
---
+++ src/main/java/excelproject/FormTest.java
@@ -0,0 +1,57 @@
+package excelproject;
+
+
+import java.time.Duration;
+import java.util.concurrent.TimeoutException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+import org.testng.annotations.DataProvider;
+
+public class FormTest {
+	@Test(dataProvider = "driveTest", dataProviderClass = DataProvide.class)
+	public void fillForm(String testType, String idMonth, String idDay, String idYear, String borough, String block,
+			String lots, String address, String contactUmb, String email, String filePath) throws InterruptedException {
+			
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://outreached.wufoo.com/forms/whv8t011qzb1r7/");
+		driver.manage().window().maximize();
+		Select select = new Select(driver.findElement(By.id("Field50")));
+		select.selectByVisibleText(testType);
+		driver.findElement(By.id("Field1-1")).sendKeys(idMonth);
+		driver.findElement(By.id("Field1-2")).sendKeys(idDay);
+		driver.findElement(By.id("Field1")).sendKeys(idYear);
+		Select s = new Select(driver.findElement(By.id("Field88")));
+		s.selectByVisibleText(borough);
+		
+		driver.findElement(By.id("Field94")).sendKeys(block);
+		driver.findElement(By.id("Field96")).sendKeys(lots);
+		driver.findElement(By.id("Field92")).sendKeys(address);
+		driver.findElement(By.id("Field17")).sendKeys(contactUmb.substring(0, 3));
+		driver.findElement(By.id("Field17-1")).sendKeys(contactUmb.substring(3, 6));
+		driver.findElement(By.id("Field17-2")).sendKeys(contactUmb.substring(6, 10));
+		driver.findElement(By.id("Field18")).sendKeys(email);
+		if(filePath!="") {
+			driver.findElement(By.id("Field45")).sendKeys(filePath);
+
+		}
+		//driver.findElement(By.id("saveForm")).click();
+//		String targetURL = "https://outreached.wufoo.com/confirm/backflow-test-form-online-submission/";
+//		try {
+//		    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+//		    wait.until(ExpectedConditions.urlToBe(targetURL));
+//		    
+//		} catch (Exception e) {
+//
+//		   System.exit(0);
+//		}
+
+	    driver.quit();
+	}
+}
